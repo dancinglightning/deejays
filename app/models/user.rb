2870bcd8b703f5c181a9130e00f9c24a1979ad70
Torsten Rüger <torsten@rubydesign.fi>
2020-01-26 20:25:25 +0700

    fighting the bot

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,6 +11,8 @@ class User < ActiveRecord::Base
 
   validate :has_http
   validate :is_alpha
+  validate :is_minsk
+  validate :capitals
 
   has_many :songs
   scope :admin, -> { where(admin: '1') }
@@ -26,5 +28,23 @@ class User < ActiveRecord::Base
   def is_alpha
     return unless self.full_name.match( /\d/)
     errors.add(:full_name , "No digits allowed in names")
+  end
+  def is_minsk
+    return unless self.city == "Minsk"
+    return if self.country == "Belarus"
+    errors.add(:city , "Me thinks you may live in Belarus")
+  end
+  def capitals
+    [:city, :full_name, :country , :state].each do |attr|
+      errors.add(attr , "You have your caps lock on") if(has_caps(attr) > 2)
+    end
+  end
+  def has_caps(attr)
+    value = self.send(attr)&.delete(" ")
+    return 0 if value.blank?
+    up = value.upcase
+    total = 0
+    (0 ... value.length).each {|i| total += 1 if value[i] == up[i]}
+    total
   end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -32,4 +32,16 @@ describe User do
     user.save
     expect(user.errors[:full_name].to_s).to include "digits"
   end
+  it "validates minsk" do
+    user = build :user , city: "Minsk" , country: "Haiti"
+    user.save
+    expect(user.errors[:city].to_s).to include "Belarus"
+  end
+  it "validates caps" do
+    [:city, :full_name, :country , :state].each do |attr|
+      user = build :user , attr => "GroSS"
+      user.save
+      expect(user.errors[attr].to_s).to include "lock"
+    end
+  end
 end
